b29b44cf553d6a6e425ea0b579da635904892986
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-15 09:51:20 +0200

    Renamed key_handler to key_handle

--- src/game/key_handle.c
+++ src/game/key_handle.c
@@ -1,17 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   key_handler.c                                      :+:      :+:    :+:   */
+/*   key_handle.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 09:00:26 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 06:02:06 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/15 09:47:49 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 #include "keybinding.h"
+#include <X11/keysymdef.h>
 
 int	key_handle(int key)
 {
@@ -31,4 +32,5 @@ int	key_handle(int key)
 	if (key == KEY_RIGHT)
 		++motion_x;
 	player_move(motion_x, motion_y);
+	return (0);
 }
