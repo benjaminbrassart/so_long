d366895a5e0ebf425aa83a0dcd51a394cb582bb0
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-15 07:24:09 +0200

    Freed map tiles in slexit

--- src/game/slexit.c
+++ src/game/slexit.c
@@ -6,12 +6,13 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 05:20:57 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 05:44:51 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/15 06:46:59 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdio.h"
 #include "slerror.h"
+#include "map.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -20,13 +21,14 @@ static char const	*_get_errno_message(t_errno err)
 	int	i;
 
 	i = 0;
-	while (g_errno[i].errno != NONE && g_errno[i].errno != err)
+	while (g_errno[i].errnum != NONE && g_errno[i].errnum != err)
 		++i;
 	return (g_errno[i].message);
 }
 
 static void	_slexit(char const *message)
 {
+	free(_map()->tiles);
 	if (message)
 	{
 		ft_dprintf(2, "Error: %s\n", message);
