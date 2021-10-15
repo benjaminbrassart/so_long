1cd97c307ed0a25d6fb77c8237973f441199dd90
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-15 09:52:38 +0200

    Changed map_get_tile coordinates to unsigned ints

--- include/map.h
+++ include/map.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 08:37:33 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 07:09:51 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/15 09:39:07 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,11 @@ t_map	*_map(void);
 
 void	map_check(void);
 
-t_tile	map_get_tile(t_map *map, int x, int y);
+void	map_draw(void);
+
+void	map_draw_tile(unsigned int x, unsigned int y);
+
+t_tile	map_get_tile(t_map *map, unsigned int x, unsigned int y);
 
 void	map_load(int fd);
 
--- src/map/map_get_tile.c
+++ src/map/map_get_tile.c
@@ -6,16 +6,16 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 05:05:08 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 05:15:02 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/15 09:31:39 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
 #include "tiles.h"
 
-t_tile	map_get_tile(t_map *map, int x, int y)
+t_tile	map_get_tile(t_map *map, unsigned int x, unsigned int y)
 {
-	if (x < 0 || y < 0 || x >= map->width || y >= map->height)
+	if (x >= map->width || y >= map->height)
 		return (WALL);
 	return (map->tiles[y * map->width + x]);
 }
