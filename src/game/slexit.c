f660df71b9fa70cfdc628ea489a856833369385f
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-15 09:53:37 +0200

    Added window and display destruction in slexit

--- src/game/slexit.c
+++ src/game/slexit.c
@@ -6,13 +6,15 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 05:20:57 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 06:46:59 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/15 09:41:50 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdio.h"
-#include "slerror.h"
+#include "game.h"
 #include "map.h"
+#include "mlx.h"
+#include "slerror.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -28,6 +30,10 @@ static char const	*_get_errno_message(t_errno err)
 
 static void	_slexit(char const *message)
 {
+	t_game *const	game = _game();
+
+	mlx_destroy_window(game->mlx, game->win);
+	mlx_destroy_display(game->mlx);
 	free(_map()->tiles);
 	if (message)
 	{
