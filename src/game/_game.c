f0a0dff51cd04f86770e4f3136da0f66a959318d
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-15 09:51:28 +0200

    Fixed _game

--- src/game/_game.c
+++ src/game/_game.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 09:17:51 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 05:08:06 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/15 09:27:29 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ t_game	*_game(void)
 {
 	static t_game	game = {FT_NULL, FT_NULL};
 
-	return (&_game);
+	return (&game);
 }
