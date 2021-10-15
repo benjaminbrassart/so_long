0c4ee90721cb768282c895222eebfc786bc31892
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-15 09:54:10 +0200

    Removed unused variable and incremented moves in player_move

--- src/player/player_move.c
+++ src/player/player_move.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 09:07:47 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 05:15:21 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/15 09:31:14 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ void	player_move(int motion_x, int motion_y)
 	t_player *const	player = _player();
 	int const		x = player->position_x + motion_x;
 	int const		y = player->position_y + motion_y;
-	t_tile			tile;
 
 	if (!(motion_x || motion_y) || map_get_tile(_map(), x, y) == WALL)
 		return ;
 	player->position_x = x;
 	player->position_y = y;
+	++(player->moves);
 }
