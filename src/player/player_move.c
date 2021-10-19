d5617ba6a942c5f75ddfcadbee2f8db4ac81e5a1
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-19 09:30:20 +0200

    Finished player_move

--- src/player/player_move.c
+++ src/player/player_move.c
@@ -6,23 +6,43 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 09:07:47 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 09:31:14 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/19 09:29:38 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_stdio.h"
 #include "game.h"
 #include "map.h"
 #include "player.h"
+
+static void	_check_tile(t_tile tile, int x, int y)
+{
+	t_player *const	player = _player();
+
+	if (tile == ITEM)
+	{
+		map_set_tile(x, y, EMPTY);
+		++player->collectibles;
+	}
+	if (tile == EXIT && player->collectibles == _map()->collectibles)
+		slexit(NONE);
+}
 
 void	player_move(int motion_x, int motion_y)
 {
 	t_player *const	player = _player();
 	int const		x = player->position_x + motion_x;
 	int const		y = player->position_y + motion_y;
+	t_tile const	tile = map_get_tile(x, y);
 
-	if (!(motion_x || motion_y) || map_get_tile(_map(), x, y) == WALL)
+	if (!(motion_x || motion_y) || tile == WALL)
 		return ;
 	player->position_x = x;
 	player->position_y = y;
 	++(player->moves);
+	_check_tile(tile, x, y);
+	map_draw_tile(x - motion_x, y - motion_y);
+	map_draw_tile(x, y);
+	player_draw();
+	update_moves();
 }
