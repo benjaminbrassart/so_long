51273bdffff4c9f3ab146bf3a1a7ed219c1c4043
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-19 09:32:52 +0200

    Added key masks to window_init

--- src/game/window_init.c
+++ src/game/window_init.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 09:22:19 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/19 04:28:19 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/19 08:19:58 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,10 @@ void	window_init(void)
 	t_game *const	game = _game();
 	t_map *const	map = _map();
 
-	game->win = mlx_new_window(game->mlx, map->width * 32, map->height * 32 + 32,
-			WIN_TITLE);
+	game->win = mlx_new_window(game->mlx, map->width * 32,
+			map->height * 32 + 16, WIN_TITLE);
 	if (!game->win)
 		slexit(WINDOW_INIT);
-	map_draw();
-	mlx_key_hook(game->win, key_handle, FT_NULL);
+	mlx_hook(game->win, 2, (1 << 0) | (1 << 1), key_handle, FT_NULL);
 	mlx_hook(game->win, 17, 0, destroy_handle, FT_NULL);
 }
