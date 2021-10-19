157c7c0a887f64c26c836de55f2e0092f18cfe0e
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-19 09:30:01 +0200

    Added some checks in map_check

--- include/map.h
+++ include/map.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 08:37:33 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 09:39:07 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/19 07:26:59 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 
 # include "tiles.h"
 
+typedef struct s_map_tiles
+{
+	int	spawns;
+	int	exits;
+	int	collectibles;
+}	t_map_tiles;
+
 typedef struct s_map
 {
 	unsigned int	width;
 	unsigned int	height;
 	char			*tiles;
-	int				spawn_x;
-	int				spawn_y;
-	int				collectibles;
+	unsigned int	spawn_x;
+	unsigned int	spawn_y;
+	unsigned int	collectibles;
 }	t_map;
 
 t_map	*_map(void);
@@ -33,8 +40,12 @@ void	map_draw(void);
 
 void	map_draw_tile(unsigned int x, unsigned int y);
 
-t_tile	map_get_tile(t_map *map, unsigned int x, unsigned int y);
+t_tile	map_get_tile(unsigned int x, unsigned int y);
 
 void	map_load(int fd);
+
+void	map_set_spawn(unsigned int x, unsigned int y);
+
+void	map_set_tile(unsigned int x, unsigned int y, t_tile tile);
 
 #endif
--- src/map/map_check.c
+++ src/map/map_check.c
@@ -6,13 +6,34 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 06:51:04 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 07:21:18 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/19 08:13:09 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_stdio.h"
+#include "ft_string.h"
 #include "map.h"
 #include "slerror.h"
+
+static void	_update_map_props(char tile, t_map_tiles *mt)
+{
+	if (tile == SPAWN && ++mt->spawns > 1)
+		slexit(MAP_SPAWNS);
+	if (tile == ITEM)
+		++mt->collectibles;
+	if (tile == EXIT)
+		++mt->exits;
+}
+
+static void	_check_map_props(t_map_tiles *mt)
+{
+	if (mt->spawns != 1)
+		slexit(MAP_SPAWNS);
+	if (mt->collectibles == 0)
+		slexit(MAP_COLLECTIBLES);
+	if (mt->exits == 0)
+		slexit(MAP_EXITS);
+	_map()->collectibles = mt->collectibles;
+}
 
 void	map_check(void)
 {
@@ -20,19 +41,25 @@ void	map_check(void)
 	char const		*tiles = map->tiles;
 	unsigned int	x;
 	unsigned int	y;
+	t_map_tiles		mt;
 
+	ft_bzero(&mt, sizeof (mt));
 	y = 0;
 	while (y < map->height)
 	{
 		x = 0;
 		while (x < map->width)
 		{
-			if (((y == 0 || y == map->height - 1 )
+			if (((y == 0 || y == map->height - 1)
 					|| (x == 0 || x == map->width - 1))
 				&& tiles[map->width * y + x] != WALL)
 				slexit(MAP_WALLS);
+			_update_map_props(tiles[map->width * y + x], &mt);
+			if (tiles[map->width * y + x] == SPAWN)
+				map_set_spawn(x, y);
 			++x;
 		}
 		++y;
 	}
+	_check_map_props(&mt);
 }
