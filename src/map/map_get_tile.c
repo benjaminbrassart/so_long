51fd33c52700e6bd2cc6444b41d04be12b10eebf
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-19 09:30:43 +0200

    map_get_tile now uses global map

--- src/map/map_get_tile.c
+++ src/map/map_get_tile.c
@@ -6,15 +6,17 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 05:05:08 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 09:31:39 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/16 06:54:10 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
 #include "tiles.h"
 
-t_tile	map_get_tile(t_map *map, unsigned int x, unsigned int y)
+t_tile	map_get_tile(unsigned int x, unsigned int y)
 {
+	t_map *const	map = _map();
+
 	if (x >= map->width || y >= map->height)
 		return (WALL);
 	return (map->tiles[y * map->width + x]);
