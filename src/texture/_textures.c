3ab20cd43e9f99270cabf90dbc4fb8a435db5e96
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-09 15:16:40 +0100

    Removed some tabs to pass norminette 3.3.51

--- src/texture/_textures.c
+++ src/texture/_textures.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 10:03:29 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/08 11:23:53 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/09 15:05:45 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@
 t_texture	*_textures(void)
 {
 	static t_texture	images[] = {
-		{T_ITEM, "./assets/texture/collectible.xpm", FT_NULL},
-		{T_EXIT, "./assets/texture/exit.xpm", FT_NULL},
-		{T_EMPTY, "./assets/texture/ground.xpm", FT_NULL},
-		{T_WALL, "./assets/texture/wall.xpm", FT_NULL},
-		{T_PLAYER_UP, "./assets/texture/player_up.xpm", FT_NULL},
-		{T_PLAYER_LEFT, "./assets/texture/player_left.xpm", FT_NULL},
-		{T_PLAYER_DOWN, "./assets/texture/player_down.xpm", FT_NULL},
-		{T_PLAYER_RIGHT, "./assets/texture/player_right.xpm", FT_NULL},
-		{T_NONE, FT_NULL, FT_NULL},
+	{T_ITEM, "./assets/texture/collectible.xpm", FT_NULL},
+	{T_EXIT, "./assets/texture/exit.xpm", FT_NULL},
+	{T_EMPTY, "./assets/texture/ground.xpm", FT_NULL},
+	{T_WALL, "./assets/texture/wall.xpm", FT_NULL},
+	{T_PLAYER_UP, "./assets/texture/player_up.xpm", FT_NULL},
+	{T_PLAYER_LEFT, "./assets/texture/player_left.xpm", FT_NULL},
+	{T_PLAYER_DOWN, "./assets/texture/player_down.xpm", FT_NULL},
+	{T_PLAYER_RIGHT, "./assets/texture/player_right.xpm", FT_NULL},
+	{T_NONE, FT_NULL, FT_NULL},
 	};
 
 	return (images);
--- src/tile/_tile_textures.c
+++ src/tile/_tile_textures.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 06:43:18 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/08 10:19:57 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/09 15:05:48 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 t_tile_texture	*_tile_textures(void)
 {
 	static t_tile_texture	tile_textures[] = {
-		{T_ITEM, ITEM},
-		{T_EXIT, EXIT},
-		{T_EMPTY, EMPTY},
-		{T_EMPTY, SPAWN},
-		{T_WALL, WALL},
-		{T_NONE, ERROR}
+	{T_ITEM, ITEM},
+	{T_EXIT, EXIT},
+	{T_EMPTY, EMPTY},
+	{T_EMPTY, SPAWN},
+	{T_WALL, WALL},
+	{T_NONE, ERROR}
 	};
 
 	return (tile_textures);
