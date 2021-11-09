c83ef4a037398f07a30ce83b00c3ab81409685d7
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-09 15:15:28 +0100

    Removed errno global variable

--- include/slerror.h
+++ include/slerror.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 05:22:08 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/19 06:35:25 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/09 15:06:41 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,22 +36,6 @@ struct s_errno_lt
 {
 	t_errno		errnum;
 	char const	*message;
-};
-
-static const struct s_errno_lt	g_errno[] = {
-	{MALLOC_FAILED, "Memory allocation failed"},
-	{ARGUMENT_COUNT, "Expected 1 argument"},
-	{FILE_EXTENSION, "Map file extension must be '.ber'"},
-	{MAP_SHAPE, "Map must be rectangular"},
-	{MAP_WALLS, "Map must be surrounded by walls"},
-	{MAP_EXITS, "Map must have at least 1 exit"},
-	{MAP_COLLECTIBLES, "Map must have at least 1 collectible"},
-	{MAP_SPAWNS, "Map must have 1 spawn"},
-	{MLX_INIT, "Failed to initialize display"},
-	{WINDOW_INIT, "Failed to initialize window"},
-	{IMAGE_LOAD, "Failed to load image"},
-	{TEXTURE_DIMENSIONS, "Texture dimensions must be 32 by 32 pixels"},
-	{NONE, FT_NULL}
 };
 
 void	slexit(t_errno err);
--- src/game/slexit.c
+++ src/game/slexit.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 05:20:57 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/15 10:56:07 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/09 15:09:19 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+static struct s_errno_lt	*_get_errno(void)
+{
+	static struct s_errno_lt	errno_lt[] = {
+	{MALLOC_FAILED, "Memory allocation failed"},
+	{ARGUMENT_COUNT, "Expected 1 argument"},
+	{FILE_EXTENSION, "Map file extension must be '.ber'"},
+	{MAP_SHAPE, "Map must be rectangular"},
+	{MAP_WALLS, "Map must be surrounded by walls"},
+	{MAP_EXITS, "Map must have at least 1 exit"},
+	{MAP_COLLECTIBLES, "Map must have at least 1 collectible"},
+	{MAP_SPAWNS, "Map must have 1 spawn"},
+	{MLX_INIT, "Failed to initialize display"},
+	{WINDOW_INIT, "Failed to initialize window"},
+	{IMAGE_LOAD, "Failed to load image"},
+	{TEXTURE_DIMENSIONS, "Texture dimensions must be 32 by 32 pixels"},
+	{NONE, FT_NULL}
+	};
+
+	return (errno_lt);
+}
+
 static char const	*_get_errno_message(t_errno err)
 {
-	int	i;
+	struct s_errno_lt *const	g_errno = _get_errno();
+	int							i;
 
 	i = 0;
 	while (g_errno[i].errnum != NONE && g_errno[i].errnum != err)
