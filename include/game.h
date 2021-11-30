5b773e9a538ce8d19a41f458ae3c6c8bba5c3a9c
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-30 02:58:29 +0100

    Removed unused header in game.h and changed window title to "Bulbasaur!"

--- include/game.h
+++ include/game.h
@@ -6,18 +6,16 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 09:15:29 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/30 02:10:39 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/30 02:55:07 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GAME_H
 # define GAME_H
 
-# include "map.h"
-# include "player.h"
-# include "slerror.h"
+# include "types.h"
 
-# define WIN_TITLE "So long, and thanks for all the fish."
+# define WIN_TITLE "Bulbasaur!"
 # define MOVES_COLOR 0xFFFFFF
 
 t_bool	display_init(t_instance *instance);
